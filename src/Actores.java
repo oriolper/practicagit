3f14ab0d1f702c03c28924f70c562f955a2ad3df
Alex <alexgarciacarballo@hotmail.com>
2014-11-05 13:51:46 +0100

    Added functions to Actores.java

---
+++ src/Actores.java
@@ -0,0 +1,49 @@
+public class Actores {
+	private int id; 
+	private String name;
+	private String surname;
+	private int age;
+
+	public Actores(int id, String name, String surname, int age){
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+	}
+
+	public String getName() {
+	    return this.name;
+	}
+
+	public String getSurname() {
+	    return this.surname;
+	}
+
+	public int getAge() {
+	    return this.age;
+	}
+
+	public void setName(String name) {
+	    this.name = name;
+	}
+
+	public void setSurname(String surname) {
+	    this.surname = surname;
+	}
+
+	public void setAge(int age) {
+	    this.age = age;
+	}
+	public int getId() {
+	    return this.id;
+	}
+
+	public void setId(int id) {
+	    this.id = id;
+	}
+
+	public String toString(){
+		return "+id: " + this.id + "\n+Name: " + this.name + "\n+Surname: " + this.surname + "\n+age:" + this.age;
+	}
+
+}
--- src/MainApp.java
+++ src/MainApp.java
@@ -54,6 +54,18 @@ public class MainApp {
 					
 					
 					break;
+				
+				case 6:
+					//add new user
+				break;
+				
+				case 7:
+					//modify exiting actor
+				break;
+				
+				case 8:
+					//delete actor 
+				break;
 					
 			case 0:
 				break;
