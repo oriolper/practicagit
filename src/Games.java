b6a50ab3aa1032c49e3799bd47ef0e9b4d597431
manuguha <manuguha@gmail.com>
2014-11-05 13:38:45 +0100

    Added Games.java

---
+++ src/Games.java
@@ -0,0 +1,31 @@
+
+public class Games {
+
+	int id;
+	String name;
+	
+	//Constructor
+	
+	public Games(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	//getters and setters 
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
